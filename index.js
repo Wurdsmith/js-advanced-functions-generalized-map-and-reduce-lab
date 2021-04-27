72be6b477c9788a258b27bf9ec2122ace9d4ef0c
Wurdsmith <wurdsmithing@gmail.com>
2021-04-27 04:41:23 -0500

    commit tree e8a6f99cd4438f9ec9a24708bfee38034ff8fdbd

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your functions here
+function map(array, callback) {
+    const r = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      const theElement = array[i];
+      r.push(callback(theElement));
+    }
+  
+    return r;
+  }
+  
+  function reduce(array, callback, startingValue) {
+    let total;
+    if (startingValue) {
+      total = startingValue;
+      for (let i = 0; i < array.length; i++) {
+        total = callback(array[i], total);
+      }
+      return total;
+    } else {
+      total = array[0];
+      for (let i = 1; i < array.length; i++) {
+        total = callback(array[i], total);
+      }
+      return total;
+    }
+}
